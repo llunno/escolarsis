b06fd75f79e1de840dc726ad2f38612e9651fd36
Lucas Nogueira <lucaasjesusnogueira@gmail.com>
2024-06-18 12:38:37 +0000

    base commit

---
+++ src/main/java/com/infnet/edu/lucas/escolarsis/EscolarsisApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.edu.lucas.escolarsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolarsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolarsisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infnet/edu/lucas/escolarsis/EscolarsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.edu.lucas.escolarsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolarsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
