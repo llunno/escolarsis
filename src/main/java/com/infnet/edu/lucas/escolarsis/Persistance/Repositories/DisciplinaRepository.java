16dac4ac8729d884db38bb62d34b5ef2d26f56c9
Lucas Nogueira <lucaasjesusnogueira@gmail.com>
2024-06-18 13:33:01 +0000

    chore: create repositories

---
+++ src/main/java/com/infnet/edu/lucas/escolarsis/Persistance/Repositories/DisciplinaRepository.java
@@ -0,0 +1,13 @@
+package com.infnet.edu.lucas.escolarsis.Persistance.Repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infnet.edu.lucas.escolarsis.Domain.Disciplina.Disciplina;
+
+@Repository
+public interface DisciplinaRepository extends JpaRepository<Disciplina, UUID>{
+
+}
---
+++ src/main/java/com/infnet/edu/lucas/escolarsis/Persistance/Repositories/ProfessorRepository.java
@@ -0,0 +1,13 @@
+package com.infnet.edu.lucas.escolarsis.Persistance.Repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infnet.edu.lucas.escolarsis.Domain.Usuários.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, UUID>{
+
+}
