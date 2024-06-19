bc70da91826b605872217ae2b4ebe7a2741391f3
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-06-18 22:56:32 -0300

    hotfixes

--- src/main/java/com/infnet/edu/lucas/escolarsis/config/SecurityConfig.java
+++ src/main/java/com/infnet/edu/lucas/escolarsis/config/SecurityConfig.java
@@ -12,13 +12,18 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.infnet.edu.lucas.escolarsis.Business.Services.ProfessorService;
+import com.infnet.edu.lucas.escolarsis.Domain.Usuários.Professor;
 
 @Configuration
 @EnableWebSecurity
@@ -40,7 +45,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(httpRequests ->
                     httpRequests
                         .requestMatchers(
-                            "/professor/create"
+                            "/professor/create",
+                            "/professor/login",
+                            "/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
