65e9a8bf146bbc97595fca9c4d4fa258730ed6ab
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-06-24 16:42:07 +0000

    chore: change security config

--- src/main/java/com/infnet/edu/lucas/escolarsis/config/SwaggerConfig.java
+++ src/main/java/com/infnet/edu/lucas/escolarsis/config/SwaggerConfig.java
@@ -3,11 +3,18 @@ package com.infnet.edu.lucas.escolarsis.config;
 import org.springframework.stereotype.Component;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @Component
+@SecurityScheme(
+    name = "basic",
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
 @OpenAPIDefinition(
 	info = @Info(
 		title = "Escolarsis",
