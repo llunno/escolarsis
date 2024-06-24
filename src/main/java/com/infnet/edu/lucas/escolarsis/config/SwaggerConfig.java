146a9fc0f9caa4b22ce14d9693dd03c8cd4bc669
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-06-24 16:10:20 +0000

    chore: change swagger config

---
+++ src/main/java/com/infnet/edu/lucas/escolarsis/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.infnet.edu.lucas.escolarsis.config;
+
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Component
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Escolarsis",
+		version = "1.0",
+		description = "Documentação da API do Sistema de Gerenciamento Escolar",
+		summary = "Endpoints da API"
+	),
+    servers = {
+        @Server(
+            url = "https://escolarsis-production.up.railway.app",
+            description = "Local server"
+        )
+    }
+)
+public class SwaggerConfig {
+
+}
